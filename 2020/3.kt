d6d6128c7dbc16dc4298a2cfadfb5b9554775caf
Albert221 <w.albert221@gmail.com>
2020-12-05 14:30:46 +0100

    Add day 3 part two solution

--- 2020/3.kt
+++ 2020/3.kt
@@ -3,17 +3,35 @@ import java.io.File
 fun main() {
     val map = File("2020/input/3.txt").run { Map.parse(readText()) }
 
+    val partOne = treesEncountered(map, Coords(3, 1))
+
+    var partTwo = 1L
+    for (step in listOf(
+            Coords(1, 1),
+            Coords(3, 1),
+            Coords(5, 1),
+            Coords(7, 1),
+            Coords(1, 2),
+    )) {
+        partTwo *= treesEncountered(map, step)
+    }
+
+    println("Part one: %d".format(partOne)) // 211
+    println("Part two: %d".format(partTwo)) // 3584591857
+}
+
+fun treesEncountered(map: Map, step: Coords): Int {
     var trees = 0
     var currentPos = Coords(0, 0)
     while (true) {
-        if (currentPos.second == map.height) break
+        if (currentPos.second >= map.height) break
 
         if (map.isTree(currentPos)) trees++
 
-        currentPos += Coords(3, 1)
+        currentPos += step
     }
 
-    println("Part one: %d".format(trees)) // 211
+    return trees
 }
 
 typealias Coords = Pair<Int, Int>
